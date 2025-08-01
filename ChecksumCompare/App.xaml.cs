4c997b1e804780e5a1997ca892dbb93d6cba529e
Leon <lovelylayon@gmail.com>
2025-08-01 21:25:50 +0100

    chore: clean App.xaml.cs of unused libraries and redundant class

--- ChecksumCompare/App.xaml.cs
+++ ChecksumCompare/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace ChecksumCompare;
+﻿namespace ChecksumCompare;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application { }
+public partial class App { }
